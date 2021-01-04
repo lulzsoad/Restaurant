3373f3162efb8519721a42142eed6ca5846c3e20
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-01-04 20:35:01 +0100

    MenuItem Create - Post Controll created

---
+++ Restaurant/Utility/StaticDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Utility
+{
+    public static class StaticDetail
+    {
+        public const string NoImage = "no_image.jpg";
+    }
+}
