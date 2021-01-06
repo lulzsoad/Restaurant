60a64a7cea1978e971d222a19007e8a54d5da8f0
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-01-06 17:40:26 +0100

    Coupon Data Model has been created

---
+++ Restaurant/Models/Coupon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models
+{
+    public class Coupon
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string CouponType { get; set; }
+
+        public enum ECouponType { Percent=0, Value=1};
+
+        [Required]
+        public double Discount { get; set; }
+
+        [Required]
+        public double MinimumAmount { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public bool IsActive;
+    }
+}
