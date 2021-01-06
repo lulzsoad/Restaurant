c10cba765f82a6fd36c39612993048e6d9938d18
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-01-06 17:56:15 +0100

    Coupon Create View has been created

--- Restaurant/Areas/Admin/Controllers/CouponController.cs
+++ Restaurant/Areas/Admin/Controllers/CouponController.cs
@@ -22,5 +22,11 @@ namespace Restaurant.Areas.Admin.Controllers
         {
             return View(await _db.Coupon.ToListAsync());
         }
+
+        // (GET - CREATE)
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
