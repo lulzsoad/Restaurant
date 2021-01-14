f6d5c5f58799ce5058ea0f1d8ecab31b765bc544
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-01-14 21:21:01 +0100

    "Users List" View has been created with functionality of displaying the List (Works only if you're logged in)

---
+++ Restaurant/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Data;
+
+namespace Restaurant.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var claimsIndetity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIndetity.FindFirst(ClaimTypes.NameIdentifier); // FTEHCES LOGGED-IN USER
+
+
+
+            return View(await _db.ApplicationUser.Where(u=>u.Id != claim.Value).ToListAsync()); //GETS EVERY USER EXCEPT OF LOGGED IN ONE
+        }
+    }
+}
