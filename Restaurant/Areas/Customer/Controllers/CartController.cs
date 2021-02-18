bbab5f98638c5bb06107bbcc14c191b6ccc882a3
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-02-18 22:10:26 +0100

    CartController has been implemented

---
+++ Restaurant/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Data;
+using Restaurant.Models.ViewModels;
+using Restaurant.Utility;
+
+namespace Restaurant.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public OrderDetailsCartViewModel DetailCartVM { get; set; }
+
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DetailCartVM = new OrderDetailsCartViewModel()
+            {
+                OrderHeader = new Models.OrderHeader()
+            };
+
+            DetailCartVM.OrderHeader.OrderTotal = 0;
+
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var cart = _db.ShoppingCart.Where(c => c.ApplicationUserId == claim.Value);
+
+            if(cart != null)
+            {
+                DetailCartVM.ListCart = cart.ToList();
+            }
+
+            foreach(var list in DetailCartVM.ListCart)
+            {
+                list.MenuItem = await _db.MenuItem.FirstOrDefaultAsync(m => m.Id == list.MenuItemId);
+                DetailCartVM.OrderHeader.OrderTotal += (list.MenuItem.Price * list.Count);
+                list.MenuItem.Description = StaticDetail.ConvertToRawHtml(list.MenuItem.Description);
+                if(list.MenuItem.Description.Length > 100)
+                {
+                    list.MenuItem.Description = list.MenuItem.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            DetailCartVM.OrderHeader.OrderTotalOriginal = DetailCartVM.OrderHeader.OrderTotal;
+
+            return View(DetailCartVM);
+        }
+    }
+}
--- Restaurant/Utility/StaticDetail.cs
+++ Restaurant/Utility/StaticDetail.cs
@@ -13,5 +13,34 @@ namespace Restaurant.Utility
         public const string FrontDeskUser = "Obsługa";
         public const string CustomerEndUser = "Klient";
         public const string ssShoppingCartCount = "ssCartCount";
-    }
+
+
+		public static string ConvertToRawHtml(string source)
+		{
+			char[] array = new char[source.Length];
+			int arrayIndex = 0;
+			bool inside = false;
+
+			for (int i = 0; i < source.Length; i++)
+			{
+				char let = source[i];
+				if (let == '<')
+				{
+					inside = true;
+					continue;
+				}
+				if (let == '>')
+				{
+					inside = false;
+					continue;
+				}
+				if (!inside)
+				{
+					array[arrayIndex] = let;
+					arrayIndex++;
+				}
+			}
+			return new string(array, 0, arrayIndex);
+		}
+	}
 }
