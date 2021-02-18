cdebe5c2b50bfec2e83d5281c59be965a0eaf419
Łukasz Łopata <lukasz.lopata96@gmail.com>
2021-02-18 21:56:01 +0100

    OrderDetailsCartVieModel has been created for Cart View

---
+++ Restaurant/Models/ViewModels/OrderDetailsCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant.Models.ViewModels
+{
+    public class OrderDetailsCartViewModel
+    {
+        public List<ShoppingCart> ListCart { get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
+    }
+}
